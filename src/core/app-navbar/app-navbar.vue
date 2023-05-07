a066e64daf8b9a68abe53bd637d5320858f034f2
ralia <ali.roshan.nia.1379@gmail.com>
2023-05-07 12:15:25 +0330

    finish app-navbar

--- src/core/app-navbar/app-navbar.vue
+++ src/core/app-navbar/app-navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <v-app-bar elevate-on-scroll color="transparent">
-      <v-toolbar-title class="title"> PETROBUL </v-toolbar-title>
+      <v-toolbar-title class="title"> LOGO SECTON </v-toolbar-title>
 
       <v-toolbar-items>
         <ul class="d-flex align-center justify-center">
@@ -14,15 +14,23 @@
       <v-spacer></v-spacer>
       <v-toolbar-items>
         <ul class="d-flex align-center justify-center">
-          <div class="d-flex flex-row">
-            <i class="material-icons">mail</i>
-            <p>info@petrboul-jeizoel.de</p>
+          <div class="d-flex flex-row align-center">
+            <span class="material-icons d-flex align-center justify-center"
+              >alternate_email
+            </span>
+            <p class="pa-0 ma-0">info@petrboul-jeizoel.de</p>
           </div>
-          <div class="d-flex flex-row">
-            <p>022335067787</p>
+          <div class="d-flex flex-row align-center">
+            <span class="material-icons d-flex align-center justify-center"
+              >phone_in_talk</span
+            >
+            <p class="pa-0 ma-0">022335067787</p>
           </div>
-          <div class="d-flex flex-row">
-            <p>GR</p>
+          <div class="d-flex flex-row align-center">
+            <span class="material-icons d-flex align-center justify-center"
+              >flag</span
+            >
+            <p class="pa-0 ma-0">GR</p>
           </div>
         </ul>
       </v-toolbar-items>
@@ -30,7 +38,7 @@
   </div>
 </template>
 
-<style lang="scss">
+<style>
 .title {
   cursor: pointer;
 }
@@ -38,5 +46,16 @@
 ul {
   margin-left: 30px;
   gap: 2rem;
+}
+
+span {
+  width: 2rem;
+  height: 2rem;
+  color: white;
+  background-color: #ca0000;
+  padding: 5px;
+  margin: 0 5px 0 0;
+  border-radius: 50%;
+  scale: 0.7;
 }
 </style>
