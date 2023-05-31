3bc3435fcd979d1d90e9a59acc63614f6f483a47
ali.roshan.nia.1379 <ali.roshan.nia.1379@gmail.com>
2023-05-31 13:08:17 +0330

    fix in add vuetify to project

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import Vuetify from "vuetify";
-import "vuetify/dist/vuetify.min.css";
-import '../src/assets/css/main.css'
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
-
-
+import vuetify from '@/plugins/vuetify'
+import '@/assets/css/main.css'
 Vue.config.productionTip = false
 
-Vue.use(Vuetify);
 new Vue({
   router,
-  vuetify : new Vuetify() ,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
