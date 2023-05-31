9b0e571b37fffb428b17f2b5814ad39763c8db77
ali.roshan.nia.1379 <ali.roshan.nia.1379@gmail.com>
2023-05-31 14:49:37 +0330

    finish in appbar

--- src/core/app-navbar/app-navbar.vue
+++ src/core/app-navbar/app-navbar.vue
@@ -1,28 +1,28 @@
 <template>
     <div class="w-100">
-        <v-app-bar app class="hidden-sm-and-down pa-0 ma-0" height="150px" color='green' elevation="0">
+        <v-app-bar app class="hidden-sm-and-down pa-0 ma-0" height="150px" color='transparent' elevation="0">
             <v-container class="w-100 d-flex align-center justify-md-space-between h-100 pa-0 mt-0">
                 <div class="d-flex align-center col-6 fill-height pa-0">
-                    <a @click="Router().push('/').catch(()=>{})" class="col-4 d-flex align-start justify-start">
+                    <a @click="Router().push('/').catch(()=>{})" class="col-4 d-flex align-start justify-start pa-0 ma-0">
                         <img src="@/assets/img/Logo.png" alt="logo" class="fit_image"/>
                     </a>
-                    <ul class="d-flex align-center justify-space-between col-8">
+                    <ul class="d-flex align-center justify-space-between col-8  mt-7">
                         <li class="ma-0 white--text">Start</li>
                         <li class="ma-0 white--text">Heizöl bestellen</li>
                         <li class="ma-0 white--text">Über uns</li>
                         <li class="ma-0 white--text">Kontakt</li>
                     </ul>
                 </div>
-                <div class="d-flex align-center justify-space-between col-6">
-                    <div class="d-flex align-center ">
-                        <v-icon class="pa-1 rounded-pill red" color="white">mdi-home</v-icon>
+                <div class="d-flex align-center justify-end col-6 pa-0 mt-7">
+                    <div class="d-flex align-center me-5">
+                        <p class="rounded-pill custom_red_bg white--text ma-0 little_circle d-flex align-center justify-center">@</p>
                         <p class="mb-0 ms-2 white--text">info@petrobul-heizoel.de</p>
                     </div>
-                    <div class="d-flex align-center ">
-                        <v-icon class="pa-1 rounded-pill red" color="white">mdi-home</v-icon>
+                    <div class="d-flex align-center me-5">
+                        <v-icon class="pa-1 rounded-pill custom_red_bg" color="white">mdi-home</v-icon>
                         <p class="mb-0 ms-2 white--text">022335067787</p>
                     </div>
-                    <div class="d-flex align-center ">
+                    <div class="d-flex align-center">
                         <img src="@/assets/img/german_flag.svg" class="w-25" alt="german flag">
                         <p class="mb-0 ms-2 white--text">GR</p>
                     </div>
