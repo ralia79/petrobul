23d368294360ec63ea6a7278c3412ff4a5c0530f
ali.roshan.nia.1379 <ali.roshan.nia.1379@gmail.com>
2023-05-31 18:07:39 +0330

    export first section of home page to specific files

---
+++ src/core/home/first-section/first-section.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="w-100 background-truck banner-height mobile-custom-top-padding">
+        <div class="w-100 banner-height blue-bg-opacity">
+            <v-container class="d-flex flex-column-reverse flex-md-row align-start padding-custom-top">
+                <div class="col-12 col-md-7 d-flex flex-column justify-space-between mt-2 pa-0 ma-md-0">
+                    <h1 class="white--text">
+                        PETROBUL HEIZÖL
+                    </h1>
+                    <h2 class="white--text mt-5">
+                        Ihr zuverlässiger Heizöl-Partner für die Region Hürth und Umgebung
+                    </h2>
+                    <p class="white-opacity-text mt-2">
+                        Vertrauen Sie auf unsere jahrzehntelange Expertise und sichern Sie sich Energie zum
+                        Wunschtermin
+                        für Ihre individuelle und optimale Energieversorgung.
+                    </p>
+                    <div class="w-100 d-flex flex-column flex-md-row align-center mt-8">
+                        <v-btn color="#CA0000" class="d-flex align-center ps-10 pe-10 pt-7 pb-7 me-md-5 w-md-100"
+                               dark
+                               rounded>
+                            <v-icon class="ma-0 pa-0">mdi-credit-card</v-icon>
+                            <p class="ms-2 mb-0">Buy template</p>
+                        </v-btn>
+                        <v-btn class="pa-7 w-md-100 mt-5 mt-md-0" dark outlined rounded>
+                            <p class="ms-2 mb-0">Kontakt</p>
+                        </v-btn>
+                    </div>
+                </div>
+                <div class="col-md-5 d-flex align-end justify-center justify-md-end mt-2 mt-md-7 mt-md-0 pa-0 ma-0">
+                    <img src="@/assets/img/assets01.png" alt="assets" class="w-75 w-md-100">
+                </div>
+            </v-container>
+        </div>
+    </div>
+</template>
--- src/core/home/home.component.ts
+++ src/core/home/home.component.ts
@@ -1,7 +1,9 @@
 import {defineComponent} from "vue";
 import ourService from '@/core/home/our-service/our-service.vue'
+import FirstSection from "@/core/home/first-section/first-section.vue";
 export default defineComponent({
     components : {
+        FirstSection,
         ourService
     }
 })
