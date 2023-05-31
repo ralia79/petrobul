a0b5482658dda0991c5d2af1bb7042a06697883e
ali.roshan.nia.1379 <ali.roshan.nia.1379@gmail.com>
2023-05-31 12:56:44 +0330

    change in appbar

---
+++ src/core/app-navbar/app-navbar.component.ts
@@ -0,0 +1,10 @@
+import Router from "@/router";
+import {defineComponent} from "vue";
+
+export default defineComponent({
+    methods: {
+        Router() {
+            return Router
+        }
+    }
+})
--- src/core/app-navbar/app-navbar.vue
+++ src/core/app-navbar/app-navbar.vue
@@ -1,61 +1,38 @@
 <template>
-  <div class="container">
-    <v-app-bar elevate-on-scroll color="transparent">
-      <v-toolbar-title class="title"> LOGO SECTON </v-toolbar-title>
+    <div class="w-100">
+        <v-app-bar app class="hidden-sm-and-down pa-0 ma-0" height="150px" color='green' elevation="0">
+            <v-container class="w-100 d-flex align-center justify-md-space-between h-100 pa-0 mt-0">
+                <div class="d-flex align-center col-6 fill-height pa-0">
+                    <a @click="Router().push('/').catch(()=>{})" class="col-4 d-flex align-start justify-start">
+                        <img src="@/assets/img/Logo.png" alt="logo" class="fit_image"/>
+                    </a>
+                    <ul class="d-flex align-center justify-space-between col-8">
+                        <li class="ma-0 white--text">Start</li>
+                        <li class="ma-0 white--text">Heizöl bestellen</li>
+                        <li class="ma-0 white--text">Über uns</li>
+                        <li class="ma-0 white--text">Kontakt</li>
+                    </ul>
+                </div>
+                <div class="d-flex align-center justify-space-between col-6">
+                    <div class="d-flex align-center ">
+                        <v-icon color="red" class="pa-4">mdi-mail-ru</v-icon>
+                        <p class="mb-0 ms-2 white--text">info@petrobul-heizoel.de</p>
+                    </div>
+                    <div class="d-flex align-center ">
+                        <v-icon color="red" class="pa-4">mdi-mail-ru</v-icon>
+                        <p class="mb-0 ms-2 white--text">022335067787</p>
+                    </div>
+                    <div class="d-flex align-center ">
+                        <img src="@/assets/img/german_flag.svg" class="w-25" alt="german flag">
+                        <p class="mb-0 ms-2 white--text">GR</p>
+                    </div>
+                </div>
+            </v-container>
+        </v-app-bar>
+        <v-app-bar app class="hidden-md-and-up" height="230px">
 
-      <v-toolbar-items>
-        <ul class="d-flex align-center justify-center">
-          <li>Start</li>
-          <li>Heizol bestellen</li>
-          <li>Uber uns</li>
-          <li>Kontakt</li>
-        </ul>
-      </v-toolbar-items>
-      <v-spacer></v-spacer>
-      <v-toolbar-items>
-        <ul class="d-flex align-center justify-center">
-          <div class="d-flex flex-row align-center">
-            <span class="material-icons d-flex align-center justify-center"
-              >alternate_email
-            </span>
-            <p class="pa-0 ma-0">info@petrboul-jeizoel.de</p>
-          </div>
-          <div class="d-flex flex-row align-center">
-            <span class="material-icons d-flex align-center justify-center"
-              >phone_in_talk</span
-            >
-            <p class="pa-0 ma-0">022335067787</p>
-          </div>
-          <div class="d-flex flex-row align-center">
-            <span class="material-icons d-flex align-center justify-center"
-              >flag</span
-            >
-            <p class="pa-0 ma-0">GR</p>
-          </div>
-        </ul>
-      </v-toolbar-items>
-    </v-app-bar>
-  </div>
+        </v-app-bar>
+    </div>
 </template>
 
-<style>
-.title {
-  cursor: pointer;
-}
-
-ul {
-  margin-left: 30px;
-  gap: 2rem;
-}
-
-span {
-  width: 2rem;
-  height: 2rem;
-  color: white;
-  background-color: #ca0000;
-  padding: 5px;
-  margin: 0 5px 0 0;
-  border-radius: 50%;
-  scale: 0.7;
-}
-</style>
+<script src="./app-navbar.component.ts"></script>
