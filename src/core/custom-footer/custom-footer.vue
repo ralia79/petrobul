ea4fc0d5ec1d6bd93269179b9ce0c7e3b8e2d53c
ali.roshan.nia.1379 <ali.roshan.nia.1379@gmail.com>
2023-06-07 16:24:24 +0330

    finish responsive footer

--- src/core/custom-footer/custom-footer.vue
+++ src/core/custom-footer/custom-footer.vue
@@ -1,24 +1,30 @@
 <template>
-    <v-row class="ma-0 blue-bg pt-15 pb-15 footer-colors">
-        <v-container>
-            <v-row justify="space-between">
+    <v-row class="ma-0 blue-bg pt-md-15 pb-md-15 footer-colors">
+        <v-container class="pa-10 pa-md-0">
+            <v-row justify="space-between" class="flex-md-row-reverse">
                 <v-col class="col col-12 col-md-4">
-                    <v-row class="ma-0">
-                        <img src="@/assets/img/Logo-mobile.svg" alt="LOGO" class="fit_image">
+                    <v-row justify="center" align="center" class="ma-0">
+                        <h3 class="white--text text-center line-height-50">
+                            Wir versorgen Sie mit Neuigkeiten und Informationen
+                        </h3>
                     </v-row>
-                    <v-row class="ma-0 mt-5">
-                        <p class="ma-0 mt-5">
-                            Petrobul Heizöl <br>
-                            Als mittelständischer, familiengeführter Energiedienstleister aus Hürth liefert Petrobul
-                            Heizöl seit über 25 Jahren zuverlässig und termingerecht hochwertige Energieprodukte in
-                            Hürth und Umgebung.
-                        </p>
-                    </v-row>
-                    <v-row justify="start" class="mt-10 ma-0">
-                        <v-icon large color="#949494" class="me-13">mdi-facebook</v-icon>
-                        <v-icon large color="#949494" class="me-13">mdi-twitter</v-icon>
-                        <v-icon large color="#949494" class="me-13">mdi-instagram</v-icon>
-                        <v-icon large color="#949494">mdi-linkedin</v-icon>
+                    <v-row class="mt-10 mt-md-2 mb-10 mb-md-0" align="center" align-md="start" justify="center"
+                           justify-md="start">
+                        <v-col class="col col-12 col-md-8 pa-0 pe-md-3 ">
+                            <v-text-field solo
+                                          placeholder="Geben sie ihre E-Mail"
+                                          prepend-inner-icon="mdi-email"
+                                          class="rounded-text-field"
+                                          color="red"
+                            ></v-text-field>
+                        </v-col>
+                        <v-spacer></v-spacer>
+                        <v-col class="col col-7 col-md-4 pa-0">
+                            <v-btn color="#CA0000" class="pt-6 pb-6 pa-md-6 w-100 rounded-btn small-font white--text"
+                                   dark>
+                                Jetzt anfangen
+                            </v-btn>
+                        </v-col>
                     </v-row>
                 </v-col>
                 <v-col class="col col-12 col-md-4">
@@ -67,27 +73,22 @@
                     </v-row>
                 </v-col>
                 <v-col class="col col-12 col-md-4">
-                    <v-row justify="center" align="center" class="ma-0">
-                        <h3 class="white--text text-center line-height-50">
-                            Wir versorgen Sie mit Neuigkeiten und Informationen
-                        </h3>
+                    <v-row class="ma-0">
+                        <img src="@/assets/img/Logo-mobile.svg" alt="LOGO" class="footer-logo">
                     </v-row>
-                    <v-row align="start">
-                        <v-col class="col col-8 pa-0 pe-3">
-                            <v-text-field solo
-                                          placeholder="Geben sie ihre E-Mail"
-                                          prepend-inner-icon="mdi-email"
-                                          class="rounded-text-field"
-                                          color="red"
-                            ></v-text-field>
-                        </v-col>
-                        <v-spacer></v-spacer>
-                        <v-col class="col col-4 pa-0">
-                            <v-btn color="#CA0000" class="pt-5 pb-5 pa-md-6 w-100 rounded-btn small-font white--text"
-                                   dark>
-                                Jetzt anfangen
-                            </v-btn>
-                        </v-col>
+                    <v-row class="ma-0 mt-5">
+                        <p class="ma-0 mt-5">
+                            Petrobul Heizöl <br>
+                            Als mittelständischer, familiengeführter Energiedienstleister aus Hürth liefert Petrobul
+                            Heizöl seit über 25 Jahren zuverlässig und termingerecht hochwertige Energieprodukte in
+                            Hürth und Umgebung.
+                        </p>
+                    </v-row>
+                    <v-row justify="center" justify-md="start" class="mt-10 ma-0">
+                        <v-icon large class="me-md-13 social-icon-color">mdi-facebook</v-icon>
+                        <v-icon large class="me-md-13 social-icon-color">mdi-twitter</v-icon>
+                        <v-icon large class="me-md-13 social-icon-color">mdi-instagram</v-icon>
+                        <v-icon large class="social-icon-color">mdi-linkedin</v-icon>
                     </v-row>
                 </v-col>
             </v-row>
