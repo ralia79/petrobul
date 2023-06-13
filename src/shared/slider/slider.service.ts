41e29516d6b8b4a5b80e6ef6d14f15832f502189
ali.roshan.nia.1379 <ali.roshan.nia.1379@gmail.com>
2023-06-13 09:36:07 +0330

    change in google review api

--- src/shared/slider/slider.service.ts
+++ src/shared/slider/slider.service.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const placeId = 'ChIJ9TDpcNk8v0cRvZellHm-AyE';
+const placeId = 'ChIJE58IWys9v0cRoRqbVt2Gsrc';
 const apikey = 'AIzaSyD8hsEOsgequMzGwz3N3jxQd3ZtGEn1-ho';
 export default class SliderService {
     public getAllReviews(): Promise<any> {
         return new Promise<any>((resolve, reject) => {
             axios
-                .get(`https://mybusiness.googleapis.com/v4/accounts/${apikey}/locations/${placeId}/reviews`)
+                .get(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeId}&fields=user_ratings_total,rating,reviews&key=${apikey}&region=DE&language=de`)
                 .then((res: { data: any; }) => {
                     resolve(res.data);
                 })
